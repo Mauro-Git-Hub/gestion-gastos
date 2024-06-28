6c7893b802cd2b156cd822d57c879ece025486b0
Mauro Sebastian Pelegrinelli <73066069+Mauro-Git-Hub@users.noreply.github.com>
2024-06-28 08:18:55 -0300

    Create NotFound.vue

---
+++ src/components/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <navbar></navbar>
+    <h1>Página no encontrada</h1>
+    <p>La ruta que has intentado acceder no existe.</p>
+  </div>
+</template>
+
+<script>
+import Navbar from './Navbar.vue';
+
+export default {
+  components: { Navbar },
+  name: 'NotFound',
+};
+</script>
+
+<style scoped>
+/* Estilos opcionales para tu página de error */
+</style>
